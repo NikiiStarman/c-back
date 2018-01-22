782adc9de5c59d23ec92c54f2c9497b24b1b5676
Janis <janis.niklass@gmail.com>
2018-01-22 20:24:58 +0200

    Initial

--- ws-server.js
+++ ws-server.js
@@ -2,16 +2,56 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 1337 });
 
+const isValidUTF8 = require('utf-8-validate');
+
+const maxMessageLength = 280;
+
 /**
  * Helper function for escaping input strings
  */
 function htmlEntities(str) {
     return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;')
-                      .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+        .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
 }
 
-// Array with some colors
-var colors = [ 'red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange' ];
+const nicknameRegex = /^[a-z0-9]{2,10}$/i;
+function validateNickname(data) {
+    if (!isValidUTF8(Buffer.from(data))) {
+        return false;
+    }
+    let nickname = htmlEntities(data);
+    if (nickname.match(nicknameRegex) === null) {
+        return false;
+    }
+    return nickname;
+}
+
+function nicknameTaken(nickname) {
+    let result = false;
+    wss.clients.forEach(function each(client) {
+        if (client.nickname === nickname) {
+            result = true;
+        }
+    });
+    return result;
+}
+
+function validateMessage(data) {
+    if (data.length > maxMessageLength || !isValidUTF8(Buffer.from(data))) {
+        return false;
+    }
+    return htmlEntities(data);
+}
+
+function serverMessage(text) {
+    return JSON.stringify({
+        type:   'info',
+        data:   {
+            time: (new Date()).getTime(),
+            text: text
+        }
+    });
+}
 
 function noop() {}
 
@@ -22,7 +62,7 @@ function heartbeat() {
 
 wss.broadcast = function broadcast(data) {
     wss.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
+        if (client.readyState === WebSocket.OPEN && client.nickname) {
             client.send(data);
         }
     });
@@ -33,80 +73,90 @@ wss.on('connection', function connection(ws, req) {
     ws.on('pong', heartbeat);
 
     const ip = req.connection.remoteAddress;
-    // we need to know client index to remove them on 'close' event
-    // var index = wss.clients.push(connection) - 1;
-    var userName = false;
-    // var userColor = false;
-    // let userId = false;
+    let nickname = false;
 
     ws.on('message', function incoming(data) {
         console.log('received: %s', data, 'ip: ', ip);
         console.log('We have ' + wss.clients.size + ' clients');
-        // console.log(ws);
-        if (userName === false) { // first message sent by user is their name
-            // remember user name
-            userName = htmlEntities(data);
-            // get random color and send it back to the user
-            // userColor = colors.shift();
-            // userId = ids.shift();
-            wss.clients.forEach(function each(client) {
-                if (client.nickname === userName) {
-                    return ws.send(JSON.stringify({
-                        type: 'error',
-                        text: 'Failed to connect. Nickname already taken.'
-                    }))
-                }
-            });
-            ws.nickname = userName;
+
+        if (isNewUser()) {
+            let validNickname = checkProvidedNickname();
+            if (validNickname) {
+                registerUser();
+            }
+        } else {
+            let message = parseMessage();
+            if (message) {
+                wss.broadcast(message);
+            }
+        }
+
+        function isNewUser() {
+            return ws.nickname == null
+        }
+
+        function checkProvidedNickname() {
+            nickname = validateNickname(data);
+            if (nickname === false) {
+                ws.send(JSON.stringify({
+                    type: 'error',
+                    text: 'Invalid nickname. Min 2, max 10, latin characters or numbers.'
+                }));
+                return false;
+            }
+            if (nicknameTaken(nickname)) {
+                ws.send(JSON.stringify({
+                    type: 'error',
+                    text: 'Failed to connect. Nickname already taken.'
+                }));
+                return false;
+            }
+            return true;
+        }
+
+        function registerUser() {
+            ws.nickname = nickname;
             ws.send(JSON.stringify({
                 type:'user',
                 data: {
-                    name: userName
+                    name: nickname
                 }
             }));
-            // Broadcast to everyone else.
+
+            let newUserMessage = serverMessage(nickname + ' has joined');
             wss.clients.forEach(function each(client) {
-                if (client !== ws && client.readyState === WebSocket.OPEN) {
-                    client.send(JSON.stringify({ type:'info', data: {
-                            time: (new Date()).getTime(),
-                            text: userName + ' has joined'
-                        } }));
+                if (client !== ws && client.readyState === WebSocket.OPEN && client.nickname) {
+                    client.send(newUserMessage);
                 }
             });
-            console.log((new Date()) + ' User is known as: ' + userName + '.');
 
-        } else { // log and broadcast the message
-            console.log((new Date()) + ' Received Message from '
-                        + userName + ': ' + data);
-            
-            var obj = {
+            console.log((new Date()) + ' User is known as: ' + nickname + '.');
+        }
+
+        function parseMessage() {
+            let messageText = validateMessage(data);
+            if (messageText === false) {
+                ws.send(JSON.stringify({
+                    type: 'error',
+                    text: 'Invalid message. Max ' + maxMessageLength + ' characters.'
+                }));
+                return false;
+            }
+            let obj = {
                 time: (new Date()).getTime(),
                 text: htmlEntities(data),
-                author: userName
+                author: ws.nickname
             };
-            // broadcast message to all connected clients
-            var json = JSON.stringify({ type:'message', data: obj });
-            wss.broadcast(json);
-            // for (var i=0; i < wss.clients.length; i++) {
-            //     wss.clients[i].send(json);
-            // }
+            return JSON.stringify({ type:'message', data: obj });
         }
     });
 
     ws.on('close', function close() {
         console.log('disconnected');
-        if (userName !== false) {
-            console.log((new Date()) + " Peer "
-                + ip + " disconnected.");
-            // remove user from the list of connected clients
-            // wss.clients.splice(index, 1);
-            wss.broadcast(JSON.stringify({ type:'message', data: {
-                    time: (new Date()).getTime(),
-                    text: userName + ' left the chat, connection lost',
-                    author: 'Server'
-                } }));
-            // push back user's color to be reused by another user
-            // colors.push(userColor);
+        if (nickname !== false) {
+            console.log((new Date()) + " Peer " + ip + " disconnected.");
+
+            wss.broadcast(serverMessage(nickname + ' left the chat, connection lost'));
         }
     });
 
@@ -115,15 +165,15 @@ wss.on('connection', function connection(ws, req) {
         console.log(err.stack);
 
     });
-
-  // ws.send('something');
 });
 
-const interval = setInterval(function ping() {
+function pingTimeout() {
     wss.clients.forEach(function each(ws) {
         if (ws.isAlive === false) return ws.terminate();
 
         ws.isAlive = false;
         ws.ping(noop);
     });
-}, 3000);
+    setTimeout(pingTimeout, 3000)
+}
+pingTimeout();
